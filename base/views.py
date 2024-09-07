06d3295b581b4fe62629a8d553c3188b162e81e7
MinaSeiffen <minaseiffen@gmail.com>
2024-09-07 23:39:11 +0300

    CRUD Operation is now completed

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 # rooms = [
 #     {'id':1, 'name':"mina emad", },
@@ -22,3 +23,37 @@ def room(request,pk):
     room = Room.objects.get(id=pk)
     context = {'room':room}
     return render(request,'base/room.html',context)
+
+def create_room(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        # request.POST.get('name')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def update_room(request, pk):
+    room = Room.objects.get(id=int(pk))
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=int(pk))
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    context = {'obj' : room.name}
+    return render(request, 'base/delete.html' ,context )

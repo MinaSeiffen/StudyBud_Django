7a7a8581f0957053482922bd87d59b60f56685fa
MinaSeiffen <minaseiffen@gmail.com>
2024-09-07 14:35:36 +0300

    Templates learning completed

--- base/views.py
+++ base/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+    {'id':1, 'name':"mina emad", },
+    {'id':2, 'name':"mina emad lets learn something new", },
+    {'id':3, 'name':"mina emad hey", },
+    {'id':4, 'name':"mina emad lolz", },
+]
 def home(request):
+    context = {'rooms':rooms}
+    return render(request,'base/home.html' , context)
 
-    return HttpResponse("Home Page")
-
-def room(request):
-    return HttpResponse("Room Page")
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}
+    return render(request,'base/room.html',context)

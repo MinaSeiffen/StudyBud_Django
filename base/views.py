be1af01c2bb2956160d0b4ca1ae44f4607174a5f
MinaSeiffen <minaseiffen@gmail.com>
2024-09-07 16:02:31 +0300

    DataBase and Admin Panel learning are completed

--- base/views.py
+++ base/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
 
-rooms = [
-    {'id':1, 'name':"mina emad", },
-    {'id':2, 'name':"mina emad lets learn something new", },
-    {'id':3, 'name':"mina emad hey", },
-    {'id':4, 'name':"mina emad lolz", },
-]
+# rooms = [
+#     {'id':1, 'name':"mina emad", },
+#     {'id':2, 'name':"mina emad lets learn something new", },
+#     {'id':3, 'name':"mina emad hey", },
+#     {'id':4, 'name':"mina emad lolz", },
+# ]
+
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request,'base/home.html' , context)
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    # room = None
+    # rooms = Room.objects.all()
+    # for i in rooms:
+    #     if i.id == int(pk):
+    #         room = i
+    room = Room.objects.get(id=pk)
     context = {'room':room}
     return render(request,'base/room.html',context)

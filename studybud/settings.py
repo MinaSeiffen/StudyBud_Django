d920fa1be4a253bf5af8ae785e4a57c193c68c15
MinaSeiffen <minaseiffen@gmail.com>
2024-10-11 12:57:48 +0300

    REST FrameWork Done , now i`m preparing for modify the built in User model in Django

--- studybud/settings.py
+++ studybud/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'base.apps.BaseConfig',
+
+    'rest_framework',
+
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -49,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'studybud.urls'
@@ -129,3 +135,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# CORS_ALLOWED_ORIGINS << this allows only a list of urls for the frontend to access our backend endpoints
+# CORS_ALLOWED_ORIGIN_REGEXES << this allows a Regular expression of the url that is tries to access our endpoints such as (r"^https://\w+\.example\.com$",) and can be a alist of that not only one RegEx
+CORS_ALLOW_ALL_ORIGINS = True
